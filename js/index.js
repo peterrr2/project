20a7e9a22f372320a9591daee1f9ef5f884de71d
peterrr2 <s372280251@gmail.com>
2022-06-16 15:08:25 +0800

    add file

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    new fullpage('#fullpage', {
+        scrollingSpeed:1000,
+        anchors: ['page1', 'page2', 'page3', 'page4', 'footer'],
+        afterLoad: function(origin, destination, direction, trigger){
+            if(destination.index == 3){
+                origin.item.classList.remove("active");
+                origin.item.classList.remove("fp-completely")
+            }
+            else if(destination.index == 4){
+                origin.item.classList.remove("active");
+                origin.item.classList.remove("fp-completely")
+            }
+        }
+    })
+    $(".footer-toTop").on("click",function(){
+        fullpage_api.moveTo(1);
+        $(".footer").removeClass("active fp-completely");
+    })
+})
