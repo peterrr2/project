e7f59bfe18a914487772ece76b88fcbd8c010534
peterrr2 <s372280251@gmail.com>
2022-06-21 11:19:11 +0800

    add product page

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
-$(document).ready(function () {
-    new fullpage('#fullpage', {
-        scrollingSpeed:1000,
-        anchors: ['page1', 'page2', 'page3', 'page4', 'footer'],
-        afterLoad: function(origin, destination, direction, trigger){
-            if(destination.index == 3){
-                origin.item.classList.remove("active");
-                origin.item.classList.remove("fp-completely")
-            }
-            else if(destination.index == 4){
-                origin.item.classList.remove("active");
-                origin.item.classList.remove("fp-completely")
-            }
+//fullpage
+new fullpage('#fullpage', {
+    scrollingSpeed:850,
+    anchors: ['page1', 'page2', 'page3', 'page4', 'footer'],
+    afterLoad: function(origin, destination, direction, trigger){
+        if(destination.index == 3){
+            origin.item.classList.remove("active");
+            origin.item.classList.remove("fp-completely")
         }
-    })
-    $(".footer-toTop").on("click",function(){
-        fullpage_api.moveTo(1);
-        $(".footer").removeClass("active fp-completely");
-    })
+        else if(destination.index == 4){
+            origin.item.classList.remove("active");
+            origin.item.classList.remove("fp-completely")
+        }
+    }
+})
+$(".footer-toTop").on("click",function(){
+    fullpage_api.moveTo(1);
+    $(".footer").removeClass("active fp-completely");
 })
---
+++ js/product.js
@@ -0,0 +1,25 @@
+//fullpage
+new fullpage('#fullpage', {
+    scrollingSpeed:850,
+    anchors: ['OsmanthusOolong', 'Oolong', 'HoneyOolong', 'RedJade','LycheeBlackTea','LycheeBlackTea'],
+    menu:"#left_menu_bar",
+    afterLoad: function(origin, destination, direction, trigger){
+        if(destination.index == 3){
+            origin.item.classList.remove("active");
+            origin.item.classList.remove("fp-completely")
+        }
+        else if(destination.index == 4){
+            origin.item.classList.remove("active");
+            origin.item.classList.remove("fp-completely")
+        }
+    }
+})
+$(".footer-toTop").on("click",function(){
+    fullpage_api.moveTo(1);
+    $(".footer").removeClass("active fp-completely");
+})
+
+// shoppingCart
+$("#shopping-bag").on("click",function(){
+    $("#shopping-cart").toggleClass("shopping-cart-show")
+})
